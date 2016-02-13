c789748a4fdbb1a56bec9d2d1ad4830f062dc4a3
Erik Castro <erik@erikcastro.net>
2016-02-13 18:35:13 -0500

    fix file upload for dopzone js

--- tontineseed/tontineseed.py
+++ tontineseed/tontineseed.py
@@ -3,7 +3,7 @@ import pdb
 import os
 
 from bottle import route, run, template, SimpleTemplate, static_file, request, redirect
-from TransmissionClient import NoSuchTorrent, TransmissionClient
+#from TransmissionClient import NoSuchTorrent, TransmissionClient
 
 # FILE UPLOAD FROM STACK OVERFLOW
 # http://stackoverflow.com/questions/22839474/python-bottle-how-to-upload-media-files-without-dosing-the-server
@@ -29,26 +29,33 @@ def upload():
     #upload_dir = get_upload_dir_path()
     upload_dir = '/tmp/torrents/'
 
-    data = request.files.data
+    #pdb.set_trace()
+    
+    files = request.files
+    print files, type(files)
+    #data = request.files.data
 
-    if(data is not None):
-        file = data.file
-        print data.filename, type(file)
-        target_path = os.path.join(upload_dir, data.filename)
+    if(files is not None):
+        uploadfile = files.file
+        print uploadfile.filename, type(uploadfile)
+        target_path = os.path.join(upload_dir, uploadfile.filename)
         print target_path
 
+        #pdb.set_trace()
         # add Ron.Rothman's code
         data_blocks = []
-        buf = data.file.read(8192)
+        buf = uploadfile.file.read(8192)
         while buf:
             data_blocks.append(buf)
-            buf = data.file.read(8192)
+            buf = uploadfile.file.read(8192)
 
         my_file_data = ''.join(data_blocks)
 
         with open(target_path, 'wb') as tf:
             tf.write(my_file_data)
 
+    print "time to redirect"
+    redirect('/pricefile/myhash')
     return None
 
 @route('/sendtorrent', method='GET')
@@ -62,8 +69,5 @@ def pricing(filehash):
     filehash = 'cd8158937344b2a066446bed7e7a0c45214f1245'
     return template('pricing', filehash=filehash)
 
-@route('/upload')
-def upload():
-    return template('upload')
 
 run(host='localhost', port=8080)

22551edf62358a00cc791a1e829214cdde83bcec
Erik Castro <erik@erikcastro.net>
2016-02-13 22:31:06 -0500

    remove js files from master

--- assets/bootstrap/js/app.js
+++
@@ -1,8 +0,0 @@
-Dropzone.options.myUpload = {
-  init: function() {
-
-    this.on("queuecomplete", function(file) {
-      window.location = "http://localhost:8080/pricefile/jhsjhkskjswwww";
-    });
-
-}};
